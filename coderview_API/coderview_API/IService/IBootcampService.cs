93a92cf7ffaa7e66077deeb2b28e2e69d3928163
Ana Garrido <anagarridocoder@gmail.com>
2023-04-20 14:14:55 +0200

    feature/bootcamp created

--- coderview_API/coderview_API/IService/IBootcampService.cs
+++ coderview_API/coderview_API/IService/IBootcampService.cs
@@ -1,6 +1,13 @@
-﻿namespace coderview_API.IService
+﻿using Entities;
+
+namespace API.IServices
 {
     public interface IBootcampService
     {
+        int PostBootcamp(BootcampItem bootcampItem);
+
+        void UpdateBootcamp(BootcampItem bootcampItem);
+        void DeactivatedBootcamp(int id);
+        List<BootcampItem> GetAllBootcamp();
     }
 }
